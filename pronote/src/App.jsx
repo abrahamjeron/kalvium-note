4a347cd8aa13295c432031350ae04379684de7d0
abrahamjeron <abrahamjeron40@gmail.com>
2023-12-29 17:02:06 +0530

    changes done

---
+++ pronote/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import './components/editor.css'
+import Editor from "./components/ editor"
+function App() {
+  return (
+    <div>
+      <Editor />
+    </div>
+  )
+}
+
+export default App
